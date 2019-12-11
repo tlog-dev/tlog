188bab2f2143046d6062c155b00a792a1319d637
nikandfor <nikandfor@gmail.com>
2019-12-11 23:01:14 +0300

    ContextWithSpan, ContextWithRandomID

--- context.go
+++ context.go
@@ -2,10 +2,13 @@ package tlog
 
 import "context"
 
-type key struct{}
+type (
+	key     struct{}
+	spankey struct{}
+)
 
 // ContextWithID creates new context with Span ID context.Value.
-// It returns the same context if id is 0
+// It returns the same context if id is zero.
 func ContextWithID(ctx context.Context, id ID) context.Context {
 	if id == z {
 		return ctx
@@ -13,15 +16,59 @@ func ContextWithID(ctx context.Context, id ID) context.Context {
 	return context.WithValue(ctx, key{}, id)
 }
 
+// ContextWithRandomID creates new context with random Span ID context.Value.
+// May be useful to enable logging in submudules even if parent trace is not started.
+func ContextWithRandomID(ctx context.Context) context.Context {
+	id := randID()
+	return context.WithValue(ctx, key{}, id)
+}
+
+// ContextWithIDOrRandom creates new context with Span ID context.Value.
+// If id is zero new random ID is generated.
+func ContextWithIDOrRandom(ctx context.Context, id ID) context.Context {
+	if id == z {
+		id = randID()
+	}
+	return context.WithValue(ctx, key{}, id)
+}
+
+// ContextWithSpan creates new context with Span ID context.Value.
+// It returns the same context if id is zero.
+func ContextWithSpan(ctx context.Context, s Span) context.Context {
+	if s.ID == z {
+		return ctx
+	}
+	return context.WithValue(ctx, spankey{}, s)
+}
+
 // IDFromContext receives Span ID from Context.
 // It returns zero if no ID found.
 func IDFromContext(ctx context.Context) ID {
 	v := ctx.Value(key{})
-	id, _ := v.(ID)
-	return id
+	if id, ok := v.(ID); ok {
+		return id
+	}
+	v = ctx.Value(spankey{})
+	if s, ok := v.(Span); ok {
+		return s.ID
+	}
+	return z
 }
 
 // SpawnFromContext spawns new Span derived form Span ID from Context.
+// It returns empty (no-op) Span if no ID found.
+func SpanFromContext(ctx context.Context) Span {
+	if DefaultLogger == nil {
+		return Span{}
+	}
+
+	v := ctx.Value(spankey{})
+	s, _ := v.(Span)
+
+	return s
+}
+
+// SpanFromContext loads saved by ContextWithSpan Span from Context.
 // It returns empty (no-op) Span if no ID found.
 func SpawnFromContext(ctx context.Context) Span {
 	if DefaultLogger == nil {
--- context_test.go
+++ context_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestContext(t *testing.T) {
+func TestContextWithID(t *testing.T) {
 	defer func(old *Logger) {
 		DefaultLogger = old
 	}(DefaultLogger)
@@ -21,6 +21,7 @@ func TestContext(t *testing.T) {
 	tr := SpawnFromContext(ctx)
 	assert.Zero(t, tr.ID)
 
+	//
 	id := ID{10, 20}
 	ctx = ContextWithID(context.Background(), id)
 
@@ -29,12 +30,80 @@ func TestContext(t *testing.T) {
 	assert.Equal(t, id, res)
 
 	tr = SpawnFromContext(ctx)
-	if assert.NotNil(t, tr) {
+	if assert.NotZero(t, tr) {
 		assert.Equal(t, `Span 0194fdc2fa2ffcc0 par 0a14000000000000 started`+"\n", buf.String())
 	}
 
+	//
 	DefaultLogger = nil
 
 	tr = SpawnFromContext(ctx)
 	assert.Zero(t, tr.ID)
+}
+
+func TestContextWithSpan(t *testing.T) {
+	defer func(old *Logger) {
+		DefaultLogger = old
+	}(DefaultLogger)
+	randID = testRandID()
+
+	var buf bytes.Buffer
+	DefaultLogger = New(NewConsoleWriter(&buf, Lspans))
+
+	id := ID{10, 20}
+
+	ctx := ContextWithSpan(context.Background(), Span{ID: id})
+
+	tr := SpanFromContext(ctx)
+	assert.Equal(t, id, tr.ID)
+
+	res := IDFromContext(ctx)
+	assert.Equal(t, id, res)
+
+	tr = SpawnFromContext(ctx)
+	if assert.NotZero(t, tr) {
+		assert.Equal(t, `Span 0194fdc2fa2ffcc0 par 0a14000000000000 started`+"\n", buf.String())
+	}
+
+	//
+	ctx = ContextWithID(context.Background(), id)
+
+	tr = SpanFromContext(ctx)
+	assert.Zero(t, tr)
+
+	//
+	ctx = ContextWithSpan(context.Background(), Span{})
+	res = IDFromContext(ctx)
+	assert.Zero(t, res)
+
+	//
+	DefaultLogger = nil
+	ctx = ContextWithSpan(context.Background(), Span{ID: id})
+	tr = SpanFromContext(ctx)
+	assert.Zero(t, tr)
+}
+
+func TestContextWithRandom(t *testing.T) {
+	defer func(old *Logger) {
+		DefaultLogger = old
+	}(DefaultLogger)
+	randID = testRandID()
+
+	var buf bytes.Buffer
+	DefaultLogger = New(NewConsoleWriter(&buf, Lspans))
+
+	id := ID{10, 20}
+
+	ctx := ContextWithIDOrRandom(context.Background(), id)
+	res := IDFromContext(ctx)
+	assert.Equal(t, id, res)
+
+	ctx = ContextWithIDOrRandom(context.Background(), ID{})
+	res = IDFromContext(ctx)
+	assert.NotZero(t, res)
+	assert.NotEqual(t, id, res)
+
+	ctx = ContextWithRandomID(context.Background())
+	res = IDFromContext(ctx)
+	assert.NotZero(t, res)
 }
