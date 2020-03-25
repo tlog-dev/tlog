06d23b5ad3c9a06068c110813a085fae3c0373f1
nikandfor <nikandfor@gmail.com>
2020-03-25 14:37:09 +0300

    syslog example

---
+++ examples/syslog/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"log/syslog"
+
+	"github.com/nikandfor/tlog"
+)
+
+var (
+	syslogAddr  = flag.String("syslog", "/run/systemd/journal/syslog", "syslog socket address")
+	syslogProto = flag.String("syslog-proto", "", "syslog address type")
+)
+
+func main() {
+	flag.Parse()
+
+	sl, err := syslog.Dial(*syslogProto, *syslogAddr, syslog.LOG_INFO|syslog.LOG_DAEMON, "tlogdemotag")
+	if err != nil {
+		tlog.Fatalf("dial syslog: %v", err)
+	}
+
+	tlog.DefaultLogger = tlog.New(tlog.NewConsoleWriter(sl, 0))
+
+	tlog.Printf("tlog message to syslog")
+}
