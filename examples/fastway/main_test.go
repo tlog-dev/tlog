bbc0e15d2bcde9d7f12ad2247a980f1f009ed9d0
nikandfor <nikandfor@gmail.com>
2020-08-21 14:40:46 +0300

    example/fastway

---
+++ examples/fastway/main_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"testing"
+
+	"github.com/nikandfor/tlog"
+)
+
+func BenchmarkUsual(b *testing.B) {
+	b.ReportAllocs()
+
+	tlog.DefaultLogger = tlog.New(tlog.Discard)
+
+	ctx := context.Background()
+	tr := tlog.Start()
+	defer tr.Finish()
+
+	for i := 0; i < b.N; i++ {
+		usualCode(tlog.ContextWithSpan(ctx, tr), 1, 2.3)
+	}
+}
+
+func BenchmarkHot(b *testing.B) {
+	b.ReportAllocs()
+
+	tlog.DefaultLogger = tlog.New(tlog.Discard)
+
+	tr := tlog.Start()
+	defer tr.Finish()
+
+	var buf []byte
+
+	for i := 0; i < b.N; i++ {
+		buf = hotCode(tr, 4, 5.6, buf)
+	}
+}
+
+func BenchmarkFire(b *testing.B) {
+	b.ReportAllocs()
+
+	tlog.DefaultLogger = tlog.New(tlog.Discard)
+
+	tr := tlog.Start()
+	defer tr.Finish()
+
+	var buf []byte
+
+	for i := 0; i < b.N; i++ {
+		buf = fire(tr, 7, 8.9, buf)
+	}
+}
+
+func BenchmarkMadness(b *testing.B) {
+	b.ReportAllocs()
+
+	tlog.DefaultLogger = tlog.New(tlog.Discard)
+	w := tlog.NewProtoWriter(ioutil.Discard)
+
+	tr := tlog.Start()
+	defer tr.Finish()
+
+	var buf []byte
+
+	for i := 0; i < b.N; i++ {
+		buf = madness(w, tr.ID, 11, 22.33, buf)
+	}
+}
