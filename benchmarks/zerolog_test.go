69398de01692d5dbd6ad82eacd47b29f39587fdd
nikandfor <nikandfor@gmail.com>
2020-10-14 13:35:58 +0300

    zerolog bench

---
+++ benchmarks/zerolog_test.go
@@ -0,0 +1,41 @@
+package benchmarks
+
+import (
+	"testing"
+
+	"github.com/rs/zerolog"
+
+	"github.com/nikandfor/tlog"
+)
+
+func BenchmarkZerologLogger(b *testing.B) {
+	var w tlog.CountableIODiscard
+
+	l := zerolog.New(&w).With().Stack().Timestamp().Logger()
+
+	b.Run("SingleThread", func(b *testing.B) {
+		b.ReportAllocs()
+		w.N, w.B = 0, 0
+
+		for i := 0; i < b.N; i++ {
+			l.Info().Int("i", 1000+i).Msg("message")
+		}
+
+		//	w.ReportDisk(b)
+	})
+
+	b.Run("Parallel", func(b *testing.B) {
+		b.ReportAllocs()
+		w.N, w.B = 0, 0
+
+		b.RunParallel(func(b *testing.PB) {
+			i := 0
+			for b.Next() {
+				i++
+				l.Info().Int("i", 1000+i).Msg("message")
+			}
+		})
+
+		//	w.ReportDisk(b)
+	})
+}
