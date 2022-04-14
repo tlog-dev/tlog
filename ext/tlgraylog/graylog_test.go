90d46f945c92b9d7a6b00c6bf08c74256dc7ae48
nikandfor <nikandfor@gmail.com>
2022-04-14 17:50:27 +0300

    fix graylog test

--- ext/tlgraylog/graylog_test.go
+++ ext/tlgraylog/graylog_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestGraylog(t *testing.T) {
-	ts := time.Date(2022, time.January, 15, 0, 18, 12, 0, time.Local)
+	ts := time.Date(2022, time.January, 15, 0, 18, 12, 0, time.UTC)
 
 	var c, b low.Buf
 	var buf bytes.Buffer
@@ -25,7 +25,7 @@ func TestGraylog(t *testing.T) {
 	b = encode(b[:0], &ts, "a", "b", "c", 4)
 	_, err := w.Write(b)
 	assert.NoError(t, err)
-	assert.Equal(t, `{"version":"1.1","host":"myhost","timestamp":1642195092.000,"_a":"b","_c":4,"short_message":"_no_message_","level":6}`+"\n", string(c))
+	assert.Equal(t, `{"version":"1.1","host":"myhost","timestamp":1642205892.000,"_a":"b","_c":4,"short_message":"_no_message_","level":6}`+"\n", string(c))
 
 	err = json.Indent(&buf, c, "", "")
 	assert.NoError(t, err)
@@ -35,7 +35,7 @@ func TestGraylog(t *testing.T) {
 	b = encode(b[:0], &ts, "arr", []int{1, 2, 3}, "obj", encobj("one", 1, "two", 2), tlog.KeyLogLevel, tlog.Error)
 	_, err = w.Write(b)
 	assert.NoError(t, err)
-	assert.Equal(t, `{"version":"1.1","host":"myhost","timestamp":1642195092.300,"_arr":"[1,2,3]","_obj":"{one:1,two:2}","short_message":"_no_message_","level":3}`+"\n", string(c))
+	assert.Equal(t, `{"version":"1.1","host":"myhost","timestamp":1642205892.300,"_arr":"[1,2,3]","_obj":"{one:1,two:2}","short_message":"_no_message_","level":3}`+"\n", string(c))
 
 	err = json.Indent(&buf, c, "", "")
 	assert.NoError(t, err)
@@ -45,7 +45,7 @@ func TestGraylog(t *testing.T) {
 	b = encode(b[:0], &ts, tlog.KeyLabels, tlog.Labels{"a=b", "c", "ddd=eee"})
 	_, err = w.Write(b)
 	assert.NoError(t, err)
-	assert.Equal(t, `{"version":"1.1","host":"myhost","timestamp":1642195092.600,"_L_a":"b","_L_c":"","_L_ddd":"eee","short_message":"_no_message_","level":6}`+"\n", string(c))
+	assert.Equal(t, `{"version":"1.1","host":"myhost","timestamp":1642205892.600,"_L_a":"b","_L_c":"","_L_ddd":"eee","short_message":"_no_message_","level":6}`+"\n", string(c))
 
 	err = json.Indent(&buf, c, "", "")
 	assert.NoError(t, err)
